e65b4b64931e98ba850e75b666c4ad7ee0126c48
sebastianbarrozo <sebastian.barrozo@etendo.software>
2026-04-13 21:08:41 -0300

    Feature ETP-3739: Fix PWA stale cache refresh

--- tools/app-shell/vite.config.js
+++ tools/app-shell/vite.config.js
@@ -44,15 +44,6 @@ export default defineConfig(({ mode }) => {
       },
     }),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
