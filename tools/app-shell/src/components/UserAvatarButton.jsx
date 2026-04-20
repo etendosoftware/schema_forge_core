e351b87106b3db92c8ecec79be19e6644f3f91ba
Agustin-Calderon <agustin.calderon@smfconsulting.es>
2026-04-20 16:15:10 -0300

    Feature ETP-3803: Fix profile menu offset for smaller avatar button size

--- tools/app-shell/src/components/UserAvatarButton.jsx
+++ tools/app-shell/src/components/UserAvatarButton.jsx
@@ -70,7 +70,7 @@ export function UserAvatarButton({ expanded = false }) {
       <DropdownMenuContent
         side="right"
         align="end"
-        sideOffset={22}
+        sideOffset={26}
         className="w-56"
       >
         <div className="border-b px-4 py-3">
