db397d22fc93925f4c5ea091a4be483870a8b2fc
ivanrobledofutit <ivan.robledo@smfconsulting.es>
2026-04-24 15:46:59 +0000

    Feature ETP-3846: Fix defaultValue type for movementQuantity in GR and GS

--- artifacts/goods-receipt/generated/web/goods-receipt/GoodsReceiptLineForm.jsx
+++ artifacts/goods-receipt/generated/web/goods-receipt/GoodsReceiptLineForm.jsx
@@ -5,7 +5,7 @@ const fields = [
   { key: 'product', column: 'M_Product_ID', type: 'search', label: 'Product', section: 'principal', reference: 'Product', inputMode: 'search' },
   { key: 'attributeSetValue', column: 'M_AttributeSetInstance_ID', type: 'text', label: 'Attribute Set Value', section: 'principal' },
   { key: 'uOM', column: 'C_UOM_ID', type: 'selector', label: 'UOM', required: true, readOnly: true, section: 'principal', reference: 'UOM', inputMode: 'selector' },
-  { key: 'movementQuantity', column: 'MovementQty', type: 'number', label: 'Movement Quantity', required: true, section: 'principal', defaultValue: 1 },
+  { key: 'movementQuantity', column: 'MovementQty', type: 'number', label: 'Movement Quantity', required: true, section: 'principal', defaultValue: '1' },
   { key: 'storageBin', column: 'M_Locator_ID', type: 'selector', label: 'Storage Bin', section: 'principal', reference: 'Locator', inputMode: 'selector', defaultValue: '@OnHandLocatorDefault@' },
   { key: 'description', column: 'Description', type: 'textarea', label: 'Description', section: 'principal' },
 ];
