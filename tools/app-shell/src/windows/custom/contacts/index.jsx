287307b25a79fea301f4f2d84beeb11f9070c59d
ivanrobledofutit <ivan.robledo@smfconsulting.es>
2026-04-27 14:39:34 +0000

    Feature ETP-3660: Remove column borders from Contacts subtabs

--- tools/app-shell/src/windows/custom/contacts/index.jsx
+++ tools/app-shell/src/windows/custom/contacts/index.jsx
@@ -5,20 +5,12 @@ import ContactsProfileProgress from './ContactsProfileProgress';
 
 /* eslint-disable react/prop-types */
 
-const TABLE_CELL_BORDERS = [
-  '[&_thead_th]:border-r',
-  '[&_thead_th]:border-r-[#E8EAEF]',
-  '[&_tbody_td]:border-r',
-  '[&_tbody_td]:border-r-[#E8EAEF]',
-  '[&_thead_th:last-child]:border-r-0',
-  '[&_tbody_td:last-child]:border-r-0',
-  '[&_tr[data-empty-state]]:hidden',
-].join(' ');
+const CONTACTS_WRAPPER = '[&_tr[data-empty-state]]:hidden';
 
 export default function ContactsWindow(props) {
   return (
     <ContactsProvider>
-      <div className={TABLE_CELL_BORDERS}>
+      <div className={CONTACTS_WRAPPER}>
         <BusinessPartnerPage {...props} Form={ContactsBusinessPartnerForm} enableSecondaryRowDelete={true} sidebarClassName="w-[30%] shrink-0 overflow-y-auto border-l border-[#E8EAEF]" noHeaderBorder={true} toolbarBorderBottom={true} tabsBarRightDivider="30%" tabsBarRight={ContactsProfileProgress} />
       </div>
     </ContactsProvider>
